5b1696cfafa509854202b2a27905279053c4fed3
rAndrade360 <r.andrade@acad.ifma.edu.br>
2019-10-10 11:55:37 -0300

    dnd animation

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import produce from 'immer';
+import BoardContext from './context';
 import List from '../List';
 import {loadLists} from '../../services/api';
 import { Container } from './styles';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, toList, from, to){
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+    console.log(from ,to);
+  }
   return (
+    <BoardContext.Provider value={{lists, move}}>
     <Container>
-      {lists.map(list => <List key={list.title} data={list} />)}
+      {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
     </Container>
+    </BoardContext.Provider>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import { useDrag } from 'react-dnd';
-
+import React, {useRef, useContext} from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import Boardcontext from '../Board/context';
  import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index, listIndex }) {
+  const ref = useRef();
+  const {move} = useContext(Boardcontext);
   const[{isDragging}, dragRef] = useDrag({
-    item: {type: 'CARD'},
+    item: {type: 'CARD', index, listIndex},
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
+  
+
+  const [, dropRef] = useDrop({
+      accept: 'CARD',
+      hover(item, monitor){
+        const draggedListIndex = item.listIndex;
+        const targetListIndex = listIndex;
+
+
+        const targetIndex = index;
+        const draggedIndex = item.index;
+
+        if(targetIndex === draggedIndex && draggedListIndex === targetListIndex)
+          return;
+
+        const targetSize = ref.current.getBoundingClientRect();
+        const targetCenter = (targetSize.bottom - targetSize.top)/2;
+        
+        const draggedOffSet = monitor.getClientOffset();
+        const draggedTop = draggedOffSet.y - targetSize.top;
+
+        if(draggedIndex < targetIndex && draggedTop < targetCenter)
+          return;
+
+        if(draggedIndex > targetIndex && draggedTop > targetCenter)
+          return;
+
+        move(draggedListIndex, targetListIndex , draggedIndex, targetIndex);
+
+        item.index = targetIndex;
+        item.listIndex = targetListIndex;
+      }
+
+    
+  });
+
+  dragRef(dropRef(ref));
+
   return (
-    <Container isDragging={isDragging} ref={dragRef}>
+    <Container isDragging={isDragging} ref={ref}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
        
